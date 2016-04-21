bf1e9f0195dee432e566840260ce36e8eb64bf40
arey0pushpa <leadnewindia@hotmail.com>
2016-04-21 19:55:07 +0530

    close to solve

--- cellevolution.c
+++ cellevolution.c
@@ -11,19 +11,27 @@
  *    Will consider It as only One Copy. i.e we are not calculating the total number of Similar compartments present.
  *    We are taking account only total number of differnt compartments present.*/
 
-#define N 3   // Total Number of molecules
+#define N 2   // Total Number of molecules
 // State is going to be represented by M 
-#define M 8   // Total Number of different combinations i.e  
-#define true 1
-#define false 0
+#define M 4   // Total Number of different combinations i.e  
+
+
+_Bool nondet_bool();
+unsigned int nondet_uint();
 
 typedef int bool;
 typedef unsigned __CPROVER_bitvector[M] bitvector;  // define a bitvector of length N which will represent the comaprtment
 
+unsigned int nondet(){  
+    unsigned int num = nondet_uint();
+    __CPROVER_assume( num>= 0 && num  <= 1);
+    return num;
+};
+
 // Go from one transition state to updated tarnsition state using this function
 bitvector transition(bitvector state, bitvector rel , bitvector abs , bitvector getRel , bitvector getAbs ){    
     int ithofState , incState;  
-    for  (int i = 0; i < P; i++){
+    for  (int i = 0; i < M; i++){
          ithofState =  (state >> i) & 1;
          if( ithofState == 1) {
              incState = i - getRel[i];
@@ -35,11 +43,13 @@ bitvector transition(bitvector state, bitvector rel , bitvector abs , bitvector 
 
 
 int  main() {
-
-    bitvector state, l2s_s, next_state, updatevar, delvar ;
+    
     _Bool save, saved, on_loop, looped;
     _Bool C1 , C2;
+
+    bitvector state, l2s_s, next_state, updatevar, delvar ;
     unsigned int i , j , k , counter;
+    unsigned int relCount = 0, absCount = 0;
    // Each table Will have a M * M dimention. M is total number of subsets the for given N. If N = 2 i.e total 2 ^ 2 = 4 subsets.
     bitvector rel[M][M], getRel[M];  // define release table M * M
     bitvector abs[M][M], getAbs[M];  // define Absorb table M * M  
@@ -47,54 +57,71 @@ int  main() {
    // Possible values at any place of 2-Dimentional table is either have 0 or 1. 
    // 0 means not possible to make a move. 1 Means move allowed.
 
-    int* array1[P];
+    int* array1[M];
     
-    bitvector subset[P] = {0b 
-    
+    bitvector s[M] = {0b0000 ,0b0001 , 0b0010 , 0b0100 , 0b0011, 0b0101 , 0b0110 , 0b1110};
+   // bitvector n[M] = {0b0000 ,0b0001 , 0b0010 , 0b0100 , 0b0011, 0b0101 , 0b0110 , 0b1110};
+   
     C1 = 0;
     C2 = 1;
     
 
     // CONFUSION POINT HAS TO BE CLEARED ! 
     //  Allow only those places that have subset to be released
-    for(i=0;i<P;i++){
-        for(j=0;j<P;j++){
-            if(i<j && (i&j != 0 )){
-            __CPROVER_assume(rel[i][j]);
-            }
-        }
-    }  
+    /// Make absorb Absorb Correctly and Release to release correctly
     
- // Achive at most two release and at least one absorb
- 
-   for (i = 0; i < P; i++) {
-     for (j = 0; j < P; j++) {
-        C1 += (rel[i][j] ? 1 : 0);
-        C2 += (abs[i][j] ? 1 : 0);
-     }
+    /* Valid Point 
+     * Not All possible combinations are useful only 2 ^ N combination are 
+     * usefull.
+     * Sugested Solution Represent the all possible as a index and check 
+     * for the condition to be satisfied . 
+    */
 
-     __CPROVER_assume(C1 <= 2 && C1 >=1);
-     __CPROVER_assume(C2 >= 1); 
+ /* CONSTRAINTS ON RELEASE AND ABSORB TABLE 
+  *
+  * ON ABSORB TABLE 
+    At most two release and at least one absorb ans at most two absorb
+    Exactly One possiblity in a single row to make progress.
+
+ * ON RELEASE tABLE 
+ *  At least One Possiblity in a single row for the Deletion table.
+ *      Allowing multiple possible deletes. 
+    
+ *   This might be used to model the situation based on non-deterministic choice.
+ *   But in order to make this model deterministic only last choice will be taken into account.
+ 
+ */
+
+   for (i = 0; i < M; i++) {
+       for (j = 0; j < M; j++) {
+          if (i <= j) {
+             abs[i][j] = 0;
+             if ( (s[i] & s[j]) == s[j]) {
+                rel[i][j] = nondet();
+             }
+             else {
+               rel[i][j] = 0;
+             
+             }  
+          }
+
+          else {
+              rel[i][j] = 0;
+              if( (s[i] & s[j]) == s[i]) {
+                  abs[i][j] = nondet();
+              }
+              else {
+                  abs[i][j] = 0;
+              }
+          }   
+
+        relCount += (rel[i][j] ? 1 : 0);
+        absCount += (abs[i][j] ? 1 : 0);
+       }
+       __CPROVER_assume(absCount <= 2 && absCount >=1);
+       __CPROVER_assume(relCount >= 1); 
    }
  
-
-
- //Allow only subset release
-  
-
-  for(i=0;i<P;i++){
-      getRel[i] = 0;
-      getAbs[i] = 0;
-      for(j=0;j<P;j++){
-          if(rel[i][j] == 1){
-           getRel[i] =  getRel[i] || j;
-          }
-          if(abs[i][j] == 1){
-            getAbs[i] = getAbs[i] || j ;
-          }
-      }
-  }
-
  // state = 0b10111000;   //0111
   saved = 0;
   looped = 0; 						 									
